e2fc9af3b01149875da5cd3dd6ba24debe953996
CyIce <1207201395@qq.com>
2016-12-03 01:19:59 +0800

    通过Zig Zag Conversion

--- ZigZag Conversion.py
+++ ZigZag Conversion.py
@@ -13,7 +13,7 @@ row=(lens-1)//2
 #记录每两个字符的最大间隔；
 gap=numRows-1
 #记录两行之间的下标差；
-index=2*gap
+index=gap<<1
 
 if lens<numRows:
     numRows=lens
@@ -22,19 +22,17 @@ def zig():
     i=0
     while i<numRows:
         k=i
-        j=0
-        while k<lens and j<=row:
+        while k<lens:
             if i==0 or i==numRows-1:
                 ans.append(s[k])
                 k+=index
             else:
                 ans.append(s[k])
-                k+=(index-i*2)
+                k+=(index-(i<<1))
                 if k>=lens:
                     break
                 ans.append(s[k])
-                k+=2*i
-            j+=1
+                k+=(i<<1)
 
         i+=1
 
