634cd59488da10e67f9585dfde8851f6a25ba803
CyIce <1207201395@qq.com>
2016-12-03 00:11:16 +0800

    完成Zig Zag Conversion

---
+++ Add Two Numbers.py
@@ -0,0 +1,8 @@
+#Add Two Numbers
+
+class ListNode():
+    def __init__(self,x):
+        self.val=x
+        self.next=None
+
+ListNode l1=
---
+++ ZigZag Conversion.py
@@ -0,0 +1,69 @@
+# ZigZag Conversion
+
+s="AAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+
+ans=[]
+
+lens=len(s)
+
+numRows=3
+
+#记录每一行字符的个个数；
+row=(lens-1)//2
+#记录每两个字符的最大间隔；
+gap=numRows-1
+#记录两行之间的下标差；
+index=2*gap
+#记录已经排好的字符个数；
+tot=0
+
+def zig():
+    i=0
+    while i<numRows:
+        k=i-index
+        j=0
+        while j<=row:
+            if j%gap==0:
+                k+=index
+                if k>=lens:
+                    ans.append("\n")
+                    break
+                ans.append(s[k])
+                #tot+=1
+            elif (j+1)%gap==0 and i!=0 and i!=numRows-1:
+                t=k
+                k+=(index-i*2)
+                if k>=lens:
+                    ans.append("\n")
+                    break
+                print(k)
+                ans.append(s[k])
+                k=t
+                #tot+=1
+            else:
+                ans.append(" ")
+
+            if j!=row:
+                ans.append(" ")
+            else:
+                ans.append("\n")
+
+            #if tot==lens:
+                #break
+
+            j+=1
+
+        i+=1
+
+if numRows==1:
+    ans=" ".join(s)
+else:
+    zig()
+    #将ans转换为字符串；
+    ans="".join(ans)
+
+
+
+
+
+print(ans)
