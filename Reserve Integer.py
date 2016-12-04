6ef8161171508eebeed70d34bd3e46c86572c777
CyIce <1207201395@qq.com>
2016-12-04 09:57:46 +0800

    通过Reserve Integer

---
+++ Reserve Integer.py
@@ -0,0 +1,32 @@
+# Reserve Integer
+
+x=0
+
+ans=0
+
+
+def resever(num):
+    ans=[]
+    num=abs(num)
+
+    s=num//10
+    y=num%10
+
+    while num!=0:
+        y=num%10
+        ans.append(y)
+        num//=10
+    return ans
+
+num=resever(x)
+
+p=pow(10,len(num)-1)
+
+for i in num:
+    ans+=(p*i)
+    p//=10
+
+if x<0:
+    ans*=-1
+
+print(ans)
